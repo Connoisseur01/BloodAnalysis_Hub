e3c785c0086e3ba89d3ce5b742fdcf6a221571da
connoisseur01 <p.polkowski01@gmail.com>
2023-10-22 01:13:06 +0200

    Add layout template

--- app.py
+++ app.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Patryk',
+        'title': 'Post 1',
+        'content': 'blood test',
+        'date': 'October 22 2023'
+    },
+    {
+        'author': 'Michał',
+        'title': 'Post 2',
+        'content': 'different blood test',
+        'date': 'November 30 2023'
+    }
+]
+
 @app.route("/")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='about')
