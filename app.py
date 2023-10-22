4ca118b6bcd712b7b34b62641dd46db8bea23efa
connoisseur01 <p.polkowski01@gmail.com>
2023-10-22 23:39:59 +0200

    add login and register page

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'b7fea80b8940ff4a6e0cb55b70fd9849'
 
 posts = [
     {
@@ -24,3 +27,22 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='about')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "p.polkowski01@gmail.com" and form.password.data == 'password':
+            flash('successful login', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('wrong username or password', 'danger')
+    return render_template('login.html', title='Login', form=form)
