03642bb478dc9cac95c2ebdd25de33b562975918
connoisseur01 <p.polkowski01@gmail.com>
2023-10-26 21:51:12 +0200

    Add account creation with password hashing

--- blood_analysis_hub/routes.py
+++ blood_analysis_hub/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, url_for, flash, redirect
-from . import app
+from . import app, db, bcrypt
 from blood_analysis_hub.forms import RegistrationForm, LoginForm
 from blood_analysis_hub.models import User, Post
 
@@ -30,8 +30,12 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}', 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Account created successfuly! you are ready to log in', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
