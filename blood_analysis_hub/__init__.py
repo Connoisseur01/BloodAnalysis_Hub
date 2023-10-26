16b8634f10d0174211779fe345c3e4ccdc2e20c5
connoisseur01 <p.polkowski01@gmail.com>
2023-10-26 21:47:35 +0200

    add bcrypt and login manager

--- blood_analysis_hub/__init__.py
+++ blood_analysis_hub/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'b7fea80b8940ff4a6e0cb55b70fd9849'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from blood_analysis_hub import routes
