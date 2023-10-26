afd8ccc0df1fe01c2f1ffc867f033236a0658682
connoisseur01 <p.polkowski01@gmail.com>
2023-10-26 21:48:04 +0200

    Add custom validations to register form

--- blood_analysis_hub/forms.py
+++ blood_analysis_hub/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, equal_to
+from wtforms.validators import DataRequired, Length, Email, equal_to, ValidationError
+from blood_analysis_hub.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
@@ -10,6 +11,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                       validators=[DataRequired(), equal_to('password')])
     submit = SubmitField('Sign Up')
+    
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('this username is already taken')
+        
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('This email address is already in use')
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
