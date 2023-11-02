54e8964c6d59c076272f200c3ada1cc1ae021a0b
connoisseur01 <p.polkowski01@gmail.com>
2023-11-02 15:23:22 +0100

    Filter posts by current user in home

--- blood_analysis_hub/main/routes.py
+++ blood_analysis_hub/main/routes.py
@@ -1,12 +1,14 @@
 from flask import Blueprint, render_template
+from flask_login import current_user, login_required
 from blood_analysis_hub.models import Post
 
 main = Blueprint('main', __name__)
 
 @main.route("/")
 @main.route("/home")
+@login_required
 def home():
-    posts = Post.query.all()
+    posts = Post.query.filter_by(author=current_user).all()
     return render_template('home.html', posts=posts)
 
 @main.route("/about")
