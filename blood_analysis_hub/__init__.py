9ff3c6d3ee5a29caa486d544275f3164ceeb6962
connoisseur01 <p.polkowski01@gmail.com>
2023-11-02 14:00:14 +0100

    Update login view path

--- blood_analysis_hub/__init__.py
+++ blood_analysis_hub/__init__.py
@@ -9,7 +9,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 
 from blood_analysis_hub.users.routes import users
 from blood_analysis_hub.posts.routes import posts
